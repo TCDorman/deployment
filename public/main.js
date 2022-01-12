250b17b77b0609a2ecfb568cfdf6276d1d7ed942
TCDorman <timothycolindorman@gmail.com>
2022-01-12 16:18:49 -0500

    Dolphinbutton upgrade

--- public/main.js
+++ public/main.js
@@ -1,1 +1,9 @@
+const { BADHINTS } = require("dns")
+
 console.log('It deployed')
+
+const moreDolphins = document.getElementById('google')
+    moreDolphins.addEventListener('click', function(){
+        window.open('https://www.google.com/search?q=dolphin&sxsrf=AOaemvICGHu71nqtXnE-GhAE9HKKO4nE8Q:1642013561377&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjQ4aqY8az1AhVHj4kEHW-bAqAQ_AUoAXoECAIQAw&biw=972&bih=563&dpr=1#imgrc=NYcukPQs8zIdCM', '_blank')
+    });
+    
