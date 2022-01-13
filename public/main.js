8e6e5b281d910b5330b676c164b4ad76e0ab8620
TCDorman <timothycolindorman@gmail.com>
2022-01-13 15:38:05 -0500

    rollbar

--- public/main.js
+++ public/main.js
@@ -3,6 +3,7 @@ console.log('It deployed')
 const moreDolphins = document.getElementById('google')
     moreDolphins.addEventListener('click', function(){
         window.open('https://www.google.com/search?q=dolphin&sxsrf=AOaemvICGHu71nqtXnE-GhAE9HKKO4nE8Q:1642013561377&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjQ4aqY8az1AhVHj4kEHW-bAqAQ_AUoAXoECAIQAw&biw=972&bih=563&dpr=1#imgrc=NYcukPQs8zIdCM', '_blank')
+        Rollbar.warning('opened google window')
     });
 
 const popDolphins = document.getElementById('popDolphins')
--- server/index.js
+++ server/index.js
@@ -1,14 +1,29 @@
 const express = require('express')
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
+app.use(cors())
+app.use(express.static('public'))
+
+
+// include and initialize the rollbar library with your access token
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: 'c2074224c6114a5aa89f8ef6bf67699e',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
+
+// record a generic message and send it to Rollbar
+rollbar.log("Hello world!");
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../index.html'));
 });
 
 
-app.use(express.static('public'))
+
 
 const port = process.env.PORT || 4005
 
